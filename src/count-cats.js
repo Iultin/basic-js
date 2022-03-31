027236ee5e16d02a36f81b8e51098147ea72f35d
Iultin MyLapWork <iultin@mail.ru>
2022-03-31 10:05:40 +0300

    feat: added solution count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,5 +1,16 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
+// let matrix = [
+//   ["^^", ".", null, 0, false, "", NaN, "^^", 2, true, "dasdas", 1],
+//   [2, null, 0, 1, ".", "dasdas", true, NaN, "", false, "^^", "^^"],
+//   [false, ".", 1, 0, "^^", null, "", 2, "dasdas", "^^", NaN, true],
+//   [".", false, 1, null, NaN, 2, 0, "dasdas", true, "^^", "", "^^"],
+//   [false, ".", 1, 0, "^^", true, null, "^^", "", NaN, 2, "dasdas"],
+//   [false, NaN, 1, 0, ".", "^^", null, true, "dasdas", "^^", 2, ""],
+//   [null, 1, NaN, true, ".", "^^", "^^", 2, "", false, "dasdas", 0],
+//   [null, NaN, "", false, ".", 1, 0, "^^", "dasdas", true, 2, "^^"],
+// ];
+
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
@@ -14,10 +25,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix) {
+  let sample = "^^";
+  let result = 0;
+  for (let el_mat of matrix) {
+    el_mat.forEach(el => {
+      if (el === sample) result++;
+    });
+  }
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  //console.log(result);
+  return result;
 }
+
+//countCats(matrix);
+
 
 module.exports = {
   countCats
