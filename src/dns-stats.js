7abc50c3f183a2d18b6046bdfe7d7382e21257aa
Iultin MyLapWork <iultin@mail.ru>
2022-04-28 14:36:03 +0300

    feat: added solution for DNS stat

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,10 +22,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
+function getDNSStats(domains) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let result = {};
+  domains.forEach(el => {
+    el = el.split('.').map(elem => '.' + elem);
+    // console.log(el);
+    let lenEl = 0;
+    let temp = '';
+    while (lenEl < el.length) {
+      for (let i = lenEl; i < el.length; i++) {
+        temp += el[i];
+      }
+      // console.log(temp);
+      if (!(temp in result)) {
+        result[temp] = 1;
+      } else result[temp]++;
+      temp = '';
+      lenEl++;
+    }
+    // console.log(lenEl);
+  });
+  // console.log(result);
+  for (let i in result) {
+    let newKey = '.' + i.split('.').reverse().join('.');
+    newKey = newKey.slice(0, newKey.length-1);
+    // console.log(newKey);
+    if (newKey !== i) {
+      result[newKey] = result[i];
+      // console.log(result);
+      // console.log(newKey);
+      delete result[i];
+    }
+    
+    // console.log(i);
+  }
+  // result = result.split('.').reverse().join('.');
+  // console.log(result);
+  return result;
 }
+
+// getDNSStats(['epam.com']);
 
 module.exports = {
   getDNSStats
